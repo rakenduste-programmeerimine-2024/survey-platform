4b342e33f1dd7e73fbbd4c2fc25c216cdef64d31
donKapu <merimimm@tlu.ee>
2025-01-05 22:26:41 +0200

    PageBase done

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+const Kontakt = () => {
+  return (
+    <Container>
+      <Typography variant="h4" sx={{ textAlign: "center", my: 4 }}>
+        Kontakt
+      </Typography>
+    </Container>
+  );
+};
+
+export default Kontakt;
