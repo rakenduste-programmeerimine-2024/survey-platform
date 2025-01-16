a025bf9f7bb9a10291a49283d8f69122f39fcbd8
donKapu <merimimm@tlu.ee>
2025-01-16 14:10:00 +0200

    Card bg fix

--- components/about/TeamCard.tsx
+++ components/about/TeamCard.tsx
@@ -3,7 +3,13 @@ import { Card, CardContent, Typography } from "@mui/material";
 
 const TeamCard = ({ name, role }: { name: string; role: string }) => {
   return (
-    <Card sx={{ boxShadow: 3, borderRadius: 2, mb: 2 }}>
+    <Card sx={{ boxShadow: 3, borderRadius: 2, mb: 2, bgcolor: "#f5f5f5",
+      "&:hover": {
+      boxShadow: 6, 
+      transform: "translateY(-5px)", 
+      },
+      transition: "all 0.3s ease",
+    }}>
       <CardContent>
         <Typography variant="h6" fontWeight="bold">
           {name}
