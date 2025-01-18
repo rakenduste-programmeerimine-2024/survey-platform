81d80a434945ea2d7c23561dfed780d2bcfe35a2
Erik Brück <109608462+ErikBruck@users.noreply.github.com>
2025-01-18 22:57:20 +0200

    fix: removed supabase key from inside code for security reasons

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,8 @@
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL=https://pqctjehcrlhsmofmqxkz.supabase.co/
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY=uP2EF@!d8C#WgY4,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 );
 
 export default supabase;
