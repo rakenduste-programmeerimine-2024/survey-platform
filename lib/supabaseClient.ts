2891522d958901bc5655c0c7879f3f2eda3ea24a
Erik Brück <109608462+ErikBruck@users.noreply.github.com>
2025-01-18 22:37:36 +0200

    feat:added primitive supabase client

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,9 @@
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL=https://pqctjehcrlhsmofmqxkz.supabase.co/
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY=uP2EF@!d8C#WgY4,
+);
+
+export default supabase;
