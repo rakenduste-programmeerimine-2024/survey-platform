8b575345a8175b0bdfa48b2b2a96c755cd0b0167
Hannes Väster <hannesv@tlu.ee>
2025-01-19 14:26:03 +0200

    supabase

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -9,6 +9,7 @@ import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 
 export default function SignupPage() {
