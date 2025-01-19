5e06806f36f604b8a133be67270c7c2acebc09d2
Hannes Väster <hannesv@tlu.ee>
2025-01-19 14:26:23 +0200

    supabase client

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 export default function SignupPage() {
